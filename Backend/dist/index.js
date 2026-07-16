129b6b5c3ced9b922d4bf67d1239f74efa9003cc
MIRACULOUS65 <sushovan1908@gmail.com>
2026-07-17 00:13:57 +0530

    am having fun

---
+++ Backend/dist/index.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+let userCount = 0;
+let allSockets = [];
+wss.on("connection", (socket) => {
+    allSockets.push(socket);
+    userCount += 1;
+    console.log("user connected #" + userCount);
+    socket.on("message", (event) => {
+        console.log("msg received : " + event.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s?.send(event.toString());
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
